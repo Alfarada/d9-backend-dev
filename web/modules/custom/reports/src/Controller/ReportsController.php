baff543a080d0192365a2150700807ff0f6e903f
Alfredo Yepez <alfaradayepez@gmail.com>
2023-08-02 13:49:19 -0500

    fix: name of table rows
    the name of each one of the headers of the table was corrected so that it is format Ucfirst.
    
    before: firstName
    now: First Name

--- web/modules/custom/reports/src/Controller/ReportsController.php
+++ web/modules/custom/reports/src/Controller/ReportsController.php
@@ -2,11 +2,10 @@
 
 namespace Drupal\reports\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\{Url, Link};
 use Drupal\Core\Database\Database;
-use Drupal\Core\Link;
+use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ReportsController extends ControllerBase {
@@ -18,11 +17,17 @@ class ReportsController extends ControllerBase {
       $container->get('current_route_match')
     );
   }
-  public function getCustomers(): array {
 
+  public function getCustomers(): array {
     // connect to test database
     $database = Database::getConnection('default', 'db_test');
-    $fields = ['customerNumber', 'customerName', 'contactFirstName', 'contactLastName', 'phone'];
+    $fields = [
+      'customerNumber',
+      'customerName',
+      'contactFirstName',
+      'contactLastName',
+      'phone',
+    ];
     $customers = $database->select('customers', 'c')
       ->fields('c', $fields, '=')
       ->execute()
@@ -42,8 +47,8 @@ class ReportsController extends ControllerBase {
 
     $table['customers'] = [
       '#type' => 'table',
-      '#header' => $fields,
-      '#rows' => $rows
+      '#header' => $this->arrayValueFormatterUcFirst($fields),
+      '#rows' => $rows,
     ];
 
     return $table;
@@ -52,29 +57,42 @@ class ReportsController extends ControllerBase {
   public function customersDetails(): array {
     // get url parametter
     $customer_id = $this->route_match->getParameter('customer');
-
-    // conect to test database
     // connect to test database
     $database = Database::getConnection('default', 'db_test');
-    // $fields = ['customerNumber', 'customerName', 'contactFirstName', 'contactLastName', 'phone'];
-    $customers = $database->select('customers', 'c')
+    $customer = $database->select('customers', 'c')
       ->fields('c')
+      ->condition('customerNumber', $customer_id)
       ->execute()
       ->fetchAssoc();
 
-    $list = [];
-    foreach ($customers as $key => $customer) {
-      $list[] = $this->t("<strong>$key</strong>: $customer");
+    // customer properties list
+    $list_properties = [];
+    foreach ($customer as $property => $value) {
+      $property = $this->camelCaseToUcFirst($property);
+      $list_properties[] = $this->t("<strong>$property</strong>: $value");
     }
 
     $output['customer_details_list'] = [
       '#theme' => 'item_list',
-      '#items' => $list,
+      '#items' => $list_properties,
       '#title' => $this->t('Details'),
     ];
 
     return $output;
   }
 
+  public function camelCaseToUcFirst(string $string): string {
+    //validation
+    $outputString = preg_replace('/([a-z])([A-Z])/', '$1 $2', $string);
+    return ucfirst($outputString);
+  }
 
+  public function arrayValueFormatterUcFirst(array $array): array {
+    //validation
+    $array_formatted = [];
+    foreach ($array as $value) {
+      $array_formatted[] = ucfirst(preg_replace('/([a-z])([A-Z])/', '$1 $2', $value));
+    }
+    return $array_formatted;
+  }
 }
