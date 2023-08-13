0a846c0e6246a11215a2b59ea3ab69e459fd8871
Alfredo Yepez <alfaradayepez@gmail.com>
2023-08-13 10:34:45 -0500

    add new reporting table for customers
    now admin can see total amount and number of times customer paid

--- web/modules/custom/reports/src/Controller/ReportsController.php
+++ web/modules/custom/reports/src/Controller/ReportsController.php
@@ -12,7 +12,7 @@ class ReportsController extends ControllerBase {
 
   public function __construct(protected ?RouteMatchInterface $route_match) {}
 
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container): ReportsController {
     return new static(
       $container->get('current_route_match')
     );
@@ -29,14 +29,14 @@ class ReportsController extends ControllerBase {
       'phone',
     ];
     $customers = $database->select('customers', 'c')
-      ->fields('c', $fields, '=')
+      ->fields('c', $fields)
       ->execute()
       ->fetchAll();
 
     $rows = [];
     foreach ($customers as $key => $customer) {
       // link to customer details
-      $customer_url = Url::fromRoute('customers.details', ['customer' => $customer->customerNumber]);
+      $customer_url = Url::fromRoute('reports.customers_details', ['customer' => $customer->customerNumber]);
       $customer_link = Link::fromTextAndUrl($customer->customerName, $customer_url);
       $customer->customerName = $customer_link;
       // convert to array
@@ -55,7 +55,7 @@ class ReportsController extends ControllerBase {
   }
 
   public function customersDetails(): array {
-    // get url parametter
+    // get url parameter
     $customer_id = $this->route_match->getParameter('customer');
     // connect to test database
     $database = Database::getConnection('default', 'db_test');
@@ -94,5 +94,63 @@ class ReportsController extends ControllerBase {
       $array_formatted[] = ucfirst(preg_replace('/([a-z])([A-Z])/', '$1 $2', $value));
     }
     return $array_formatted;
+  }
+
+  public function getTotalPayments(): array {
+
+    $database = Database::getConnection('default', 'db_test');
+
+    $customers = $database->select('customers', 'c')
+      ->fields('c', ['customerNumber', 'customerName'])
+      ->execute()
+      ->fetchAll();
+    $customers = $this->reorder($customers);
+
+    $times_paid = $database->select('payments', 'p');
+    $times_paid->fields('p', ['customerNumber']);
+    $times_paid->addExpression('COUNT(customerNumber)', 'total_payments');
+    $times_paid->groupBy('customerNumber');
+    $times_paid->having('COUNT(customerNumber) >= 1 ');
+    $times_paid = $times_paid->execute()->fetchAll();
+    $times_paid = $this->reorder($times_paid);
+
+    $total_amount = $database->select('payments', 'p');
+    $total_amount->fields('p', ['customerNumber']);
+    $total_amount->addExpression('SUM(amount)', 'total_amount');
+    $total_amount->groupBy('customerNumber');
+    $total_amount = $total_amount->execute()->fetchAll();
+    $total_amount = $this->reorder($total_amount);
+
+    $data = [];
+    foreach ($customers as $customer) {
+      $data[$customer['customerNumber']] = [
+        $customer['customerNumber'],
+        $customer['customerName'],
+        $times_paid[$customer['customerNumber']]['total_payments'],
+        $total_amount[$customer['customerNumber']]['total_amount']
+      ];
+    }
+
+    $table['customers'] = [
+      '#type' => 'table',
+      '#header' => [
+        'Customer Number',
+        'Customer Name',
+        'Times Paid',
+        'Total Amount',
+        'Action'
+      ],
+      '#rows' => $data,
+    ];
+    return $table;
+  }
+
+  public function reorder(array $data): array {
+    $new_data = [];
+    foreach ($data as $value) {
+      $new_data[$value->customerNumber] = (array) $value;
+    }
+
+    return $new_data;
   }
 }
