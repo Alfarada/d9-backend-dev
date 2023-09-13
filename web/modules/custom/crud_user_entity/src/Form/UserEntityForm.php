d3cdf207d0922b9a6e07a027eb9e129e93b4bbc0
Alfredo Yepez <alfaradayepez@gmail.com>
2023-09-13 15:58:30 -0500

    add elements to the form
    Necessary elements are added to the form to create new user entities.

--- web/modules/custom/crud_user_entity/src/Form/UserEntityForm.php
+++ web/modules/custom/crud_user_entity/src/Form/UserEntityForm.php
@@ -2,17 +2,49 @@
 
 namespace Drupal\crud_user_entity\Form;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class UserEntityForm extends FormBase {
 
+  public function __construct(
+    protected ?EntityTypeManagerInterface $entity_type
+  ) { }
+
+  public static function create(ContainerInterface $container): self {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
   public function getFormId(): string {
     return 'crud_user_entity_form_id';
   }
 
   public function buildForm(array $form, FormStateInterface $form_state): array {
-    return ['#markup' => $this->t('test')];
+//    $users = $this->entity_type->getStorage('user');
+//    $users_collection = $users->loadMultiple(NULL);
+
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Name'),
+      '#required' => TRUE,
+      '#size' => 35
+    ];
+
+    $form['pass_confirm'] = [
+      '#type' => 'password_confirm',
+    ];
+
+    $form['mail'] = [
+      '#type' => 'email',
+      '#title' => $this->t('User email'),
+      '#required' => TRUE,
+      '#size' => 35
+    ];
+
+    return $form;
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
