d8f70864311f4ee385f51544d73a9da64b377605
Alfredo Yepez <alfaradayepez@gmail.com>
2023-10-22 15:56:10 -0500

    add baseFields
    Building the base fields that cannot be modified by the user from managing fields.

--- web/modules/custom/message/src/Entity/MessageEntity.php
+++ web/modules/custom/message/src/Entity/MessageEntity.php
@@ -4,8 +4,11 @@ namespace Drupal\message\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\Annotation\ContentEntityType;
+use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\user\UserInterface;
-use Drupal\Core\Entity\{EntityChangedTrait, EntityStorageInterface};
+use Drupal\Core\Entity\{EntityChangedTrait,
+  EntityStorageInterface,
+  EntityTypeInterface};
 
 /**
  * Defines the Message entity.
@@ -131,5 +134,111 @@ class MessageEntity extends ContentEntityBase {
     return $this;
   }
 
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
 
+    $fields = parent::baseFieldDefinitions($entity_type);
+    $fields['user_id'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Authored by'))
+      ->setDescription(t('The user ID of author of the Message entity.'))
+      ->setRevisionable(TRUE)
+      ->setSetting('target_type', 'user')
+      ->setSetting('handler', 'default')
+      ->setTranslatable(TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'author',
+        'weight' => 0,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'weight' => 5,
+        'settings' => ['match_operator' => 'CONTAINS',
+          'size' => '60',
+          'autocomplete_type' => 'tags',
+          'placeholder' => '',
+        ],
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['user_to'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('To'))
+      ->setDescription(t('The user ID of the Message recipient.'))
+      ->setRevisionable(TRUE)
+      ->setSetting('target_type', 'user')
+      ->setSetting('handler', 'default')
+      ->setTranslatable(TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'To',
+        'type' => 'author',
+        'weight' => 0,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'weight' => 5,
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => '60',
+          'autocomplete_type' => 'tags',
+          'placeholder' => '',
+        ],
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['subject'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Subject'))
+      ->setDescription(t('The subject of the Message entity.'))
+      ->setSettings( [
+        'max_length' => 100,
+        'text_processing' => 0,
+      ])
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -4,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -4,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['content'] = BaseFieldDefinition::create('text_long')
+      ->setLabel(t('Content'))
+      ->setDescription(t('The content of the Message'))
+      ->setTranslatable(TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'text_default',
+        'weight' => 0,
+      ])
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('form', ['type' => 'text_textfield',
+        'weight' => 0,
+      ])
+      ->setDisplayConfigurable('form', TRUE);
+
+    $fields['is_read'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Read'))
+      ->setDescription(t('A boolean indicating whether the Message is read.'))
+      ->setDefaultValue(FALSE);
+
+    $fields['status'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Publishing status'))
+      ->setDescription(t('A boolean indicating whether the Message is published.'))
+      ->setDefaultValue(TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t('The time that the entity was created.'));
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The time that the entity was last edited.'));
+
+    return $fields;
+  }
 }
