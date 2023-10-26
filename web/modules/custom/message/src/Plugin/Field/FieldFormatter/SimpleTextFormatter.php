52ffc3273766575c6da73e03fd6bfad280578430
Alfredo Yepez <alfaradayepez@gmail.com>
2023-10-26 14:46:53 -0500

    Add additional configuration

--- web/modules/custom/message/src/Plugin/Field/FieldFormatter/SimpleTextFormatter.php
+++ web/modules/custom/message/src/Plugin/Field/FieldFormatter/SimpleTextFormatter.php
@@ -3,9 +3,10 @@
 namespace Drupal\message\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Annotation\Translation;
-use Drupal\Core\Field\Annotation\FieldFormatter;
-use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Field\{Annotation\FieldFormatter,
+  FieldItemListInterface,
+  FormatterBase};
 
 /**
  * Plugin implementation of the 'message_entities_simple_text' formatter.
@@ -25,20 +26,47 @@ class SimpleTextFormatter extends FormatterBase {
    * @inheritDoc
    */
   public function viewElements(FieldItemListInterface $items, $langcode): array {
-
     $elements = [];
     foreach ($items as $delta => $item) {
       $elements[$delta] = [
         '#type' => 'html_tag',
-        '#tag' => 'p',
+        '#tag' => $this->getSetting('formatter_tag'),
         '#attributes' => [
-          'style' => 'color:' . $item->value
+          'style' => 'color:' . $item->value,
         ],
-        '#value' => $this->t('The color code in this field is @code', ['@code'=> $item->value]),
+        '#value' => $this->t('The color code in this field is @code', ['@code' => $item->value]),
       ];
     }
 
     return $elements;
+  }
+
+  public function settingsForm(array $form, FormStateInterface $form_state): array {
+    $output['formatter_tag'] = [
+      '#title' => $this->t('HTML tag'),
+      '#type' => 'select',
+      '#options' => [
+        'p' => $this->t('p'),
+        'div' => $this->t('div'),
+        'span' => $this->t('span'),
+      ],
+      '#default_value' => $this->getSetting('formatter_tag'),
+    ];
+
+    return $output;
+  }
+
+  public static function defaultSettings(): array {
+    return [
+        'formatter_tag' => 'p',
+      ] + parent::defaultSettings();
+  }
+
+  public function settingsSummary(): array {
+    $summary = [];
+    $formatter_tag = $this->getSetting('formatter_tag');
+    $summary[] = $this->t('HTML Tag: @tag', ['@tag' => $formatter_tag]);
+    return $summary;
   }
 
 }
