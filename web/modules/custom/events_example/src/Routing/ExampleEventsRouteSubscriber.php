d7936c9c305d7784da91b4d829642e95ce68f272
Alfredo Yepez <alfaradayepez@gmail.com>
2023-10-31 21:39:53 -0500

    Modify route through event
    Added event that renames the /user/login path to /private

---
+++ web/modules/custom/events_example/src/Routing/ExampleEventsRouteSubscriber.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\events_example\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+class ExampleEventsRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * @inheritDoc
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // change the route '/user/login' to 'private'
+    if ($route = $collection->get('user.login')) {
+      $route->setPath('/private');
+    }
+
+    // Deny access to the user profile page
+    if ($route = $collection->get('entity.user.canonical')) {
+      $route->setRequirement('_access', 'FALSE');
+    }
+  }
+
+}
