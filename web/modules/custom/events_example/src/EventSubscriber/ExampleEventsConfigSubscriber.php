46855d775ed41bc8d09281f6e6928eb46507258b
Alfredo Yepez <alfaradayepez@gmail.com>
2023-10-31 20:14:58 -0500

    add my first example subscriber event

---
+++ web/modules/custom/events_example/src/EventSubscriber/ExampleEventsConfigSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\events_example\EventSubscriber;
+
+use Drupal\Core\Config\{ConfigEvents, ConfigCrudEvent};
+use Drupal\Core\Messenger\MessengerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ExampleEventsConfigSubscriber implements EventSubscriberInterface {
+
+  public function __construct(protected MessengerInterface $messenger) {}
+
+  public function onConfigSave(ConfigCrudEvent $event): void {
+    $this->messenger->addStatus("Event: " . __FUNCTION__);
+    $config = $event->getConfig();
+    $this->messenger->addStatus("Config: " . $config->getName());
+  }
+  public function onConfigDelete(ConfigCrudEvent $event): void {
+    $this->messenger->addStatus("Event: " . __FUNCTION__);
+    $config = $event->getConfig();
+    $this->messenger->addStatus("Config: " . $config->getName());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      ConfigEvents::SAVE => ['onConfigSave'],
+      ConfigEvents::DELETE => ['onConfigDelete'],
+    ];
+  }
+}
