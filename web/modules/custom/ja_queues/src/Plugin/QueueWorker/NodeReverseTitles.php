046586a2ba45a618d7fd21a422f93834a64a468e
alfredo <alfaradayepez@gmail.com>
2023-12-27 20:14:06 -0500

    add an example queue plugin

---
+++ web/modules/custom/ja_queues/src/Plugin/QueueWorker/NodeReverseTitles.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\ja_queues\Plugin\QueueWorker;
+
+use Drupal\Core\Queue\QueueWorkerBase;
+
+/**
+ * Defines 'node_reverse_titles' queue worker.
+ *
+ * @QueueWorker (
+ *   id = "node_reverse_titles",
+ *   title = @Translation ("Node Reverse Titles"),
+ *   cron = { "time" = 5 }
+ * )
+ */
+
+class NodeReverseTitles extends QueueWorkerBase {
+
+  public function processItem(mixed $data): void {
+
+    $id = $data['id'];
+    $title = $data['title'];
+
+    // invert characters
+    $new_title = strrev($title);
+
+    $storage = \Drupal::entityTypeManager()->getStorage('node');
+
+    $node = $storage->load($id);
+    $node->setTitle($new_title);
+    $node->save();
+
+    \Drupal::logger('ja_queues')->notice('Node @id has been processed.',
+      ['@id' => $id]);
+
+    sleep(1);
+  }
+
+}
